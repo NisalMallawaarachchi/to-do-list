2401cd06098d0d47f8e6c843e6e942ac80f27788
NisalMallawaarachchi <nisalmallawarachchi@gmail.com>
2025-01-05 09:19:13 +0530

    Modify add todos -Now the user can linethrough or delete the todo

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,12 +1,22 @@
 // Check off specific todos by clicking
-$("li").click(function () {
+$("ul").on("click", "li", function () {
   $(this).toggleClass("completed");
 });
 
 // click on X do delet todo
-$("span").click(function (event) {
-  $(this).parent().fadeOut(500, function(){
-    $(this).remove();
-  });
+$("ul").on("click", "span", function (event) {
+  $(this)
+    .parent()
+    .fadeOut(500, function () {
+      $(this).remove();
+    });
   event.stopPropagation(); //stop event bubbling
+});
+
+$("input[type='text'").keypress(function (event) {
+  if (event.which === 13) {
+    var todoText = $(this).val(); // grabbing new todo text from input
+    $(this).val("");
+    $("ul").append("<li><span>X</span> " + todoText + "</li>");
+  }
 });
